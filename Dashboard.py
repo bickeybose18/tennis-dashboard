5535603498d6149ae8dccfc21f296aef482dac38
bickeybose18 <bickeybose18@gmail.com>
2026-04-05 00:05:01 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.title(" Dashboard")
 
-df = pd.read_csv("data/tennis.csv")
+df = pd.read_csv("tennis.csv")
 
 fig = px.bar(df, x="Player", y="Rank", color="Country")
 st.plotly_chart(fig, use_container_width=True)
